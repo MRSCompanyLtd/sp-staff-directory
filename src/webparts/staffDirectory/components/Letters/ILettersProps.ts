ddd5cf7a471ab8a41fbd16a5507843ce7318337c
dperk6 <daniel@perklin.ca>
2023-02-03 12:43:07 -0500

    reset letter selection on search

--- src/webparts/staffDirectory/components/Letters/ILettersProps.ts
+++ src/webparts/staffDirectory/components/Letters/ILettersProps.ts
@@ -1,4 +1,5 @@
 export interface ILettersProps {
+    letter: string | undefined;
     onLetterClick(ltr: string): void;
     reset?(): void;
 }
--- src/webparts/staffDirectory/components/Letters/Letters.tsx
+++ src/webparts/staffDirectory/components/Letters/Letters.tsx
@@ -4,20 +4,24 @@ import { ILettersProps } from "./ILettersProps";
 
 const ALPHABET: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-const Letters: React.FC<ILettersProps> = ({ onLetterClick }) => {
-    const [selected, setSelected] = React.useState<string | null>();
+const Letters: React.FC<ILettersProps> = ({ letter, onLetterClick }) => {
+    const [selected, setSelected] = React.useState<string | undefined>(letter);
 
     const handleClick: React.MouseEventHandler<HTMLDivElement> = React.useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
-        const letter: string = e.currentTarget.innerText;
+        const ltr: string = e.currentTarget.innerText;
         
-        if (letter === selected) {
-            setSelected(null);
-            onLetterClick('');
+        if (ltr === selected) {
+            setSelected(undefined);
+            onLetterClick(undefined);
         } else {
-            setSelected(letter);
-            onLetterClick(letter);
+            setSelected(ltr);
+            onLetterClick(ltr);
         }
     }, [onLetterClick, selected]);
+
+    React.useEffect(() => {
+        setSelected(letter);
+    }, [letter]);
 
     return (
         <div className={styles.letters}>
